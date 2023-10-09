d0302a354e7b448d42e310d66f0cfde1427cec65
Didier HU <didjecte@gmail.com>
2023-10-09 20:11:32 +0200

    feat: added 404 and manga nested pages

---
+++ didjecteClient/pages/manga/[name]/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex">
+      <span>Manga: {{ $route.params.name }}</span>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      mounted() {
+        const route = useRoute()
+  
+        // When accessing /posts/1, route.params.id will be 1
+        console.log(route.params)
+  
+      },
+      // created() {
+      //   this.$watch(
+      //     () => this.$route.params,
+      //     (toParams, previousParams) => {
+      //       // react to route changes...
+      //     }
+      //   )
+      // },
+      data () {
+        return {
+          visible: false,
+        }
+      }
+    }
+  </script>
